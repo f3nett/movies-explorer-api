20f38eaa96bdd02d2c300300fb93831c040c8b72
f3nett <f3nett@yandex.ru>
2022-05-29 20:14:01 +0300

    add required property to user name for signup

--- validation/signin.js
+++ validation/signin.js
@@ -11,7 +11,7 @@ const signupValidation = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
   }),
 });
 
